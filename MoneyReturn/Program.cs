4f52e383ce61c8b1c4d0fbce5722a82d80a262f2
goffa.martin.2018 <goffa.martin.2018@UC313PC03>
2022-01-25 15:09:13 +0100

    done

--- MoneyReturn/Program.cs
+++ MoneyReturn/Program.cs
@@ -7,7 +7,6 @@ namespace MoneyReturn
         static bool converted;
         static void Main(string[] args)
         {
-            int[,] moneyArray = new int[12, 2] { { 5000, 0 }, { 2000, 0 }, { 1000, 0 }, { 500, 0 }, { 200, 0 }, { 100, 0 }, { 50, 0 }, { 20, 0 }, { 10, 0 }, { 5, 0 }, { 2, 0 }, { 1, 0 } }; 
             int price = 0;
             int paid = 0;
             do
@@ -22,14 +21,22 @@ namespace MoneyReturn
             } while (!converted);
 
             int needReturn = paid - price;
+            ReturnMoney(needReturn);
+        }
+
+        static int[,] moneyArray = new int[12, 2] { { 5000, 0 }, { 2000, 0 }, { 1000, 0 }, { 500, 0 }, { 200, 0 }, { 100, 0 }, { 50, 0 }, { 20, 0 }, { 10, 0 }, { 5, 0 }, { 2, 0 }, { 1, 0 } };
+        
+        public static int[,] ReturnMoney(int needReturn)
+        {
             Console.WriteLine("Vrátit:");
             for (int i = 0; i < moneyArray.GetLength(0); i++)
             {
                 moneyArray[i, 1] = needReturn / moneyArray[i, 0];
                 needReturn %= moneyArray[i, 0];
-                if(moneyArray[i,1] != 0)
-                    Console.WriteLine($"- {moneyArray[i,0]}Kč - {moneyArray[i,1]}x");
+                if (moneyArray[i, 1] != 0)
+                    Console.WriteLine($"- {moneyArray[i, 0]}Kč - {moneyArray[i, 1]}x");
             }
+            return moneyArray;
         }
 
         public static void ConvertInt(ref int x, string sx, int min)
@@ -44,7 +51,6 @@ namespace MoneyReturn
                     Console.WriteLine("Málo peněz");
                     converted = false;
                 }
-                    
             }
             catch 
             {
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -9,10 +9,28 @@ namespace TestProject
         [TestMethod]
         public void TestConvert()
         {
+            //Arrange
             int x = 0;
             string sx = "55";
+            //Act
             Program.ConvertInt(ref x, sx, 0);
+            //Assert
             Assert.IsTrue(x == 55);
+        }
+        [TestMethod]
+        public void TestReturnMoney()
+        {
+            //Arrange
+            int returnCash = 851;
+            int[,] ControlArray = new int[12,2] { { 5000, 0 }, { 2000, 0 }, { 1000, 0 }, { 500, 1 }, { 200, 1 }, { 100, 1 }, { 50, 1 }, { 20, 0 }, { 10, 0 }, { 5, 0 }, { 2, 0 }, { 1, 1 } };
+            //Act
+            int[,] ResultArray = Program.ReturnMoney(returnCash);
+            //Assert
+            Assert.AreEqual(ControlArray[3, 1], ResultArray[3, 1]);
+            Assert.AreEqual(ControlArray[4, 1], ResultArray[4, 1]);
+            Assert.AreEqual(ControlArray[5, 1], ResultArray[5, 1]);
+            Assert.AreEqual(ControlArray[6, 1], ResultArray[6, 1]);
+            Assert.AreEqual(ControlArray[11, 1], ResultArray[11, 1]);
         }
     }
 }
